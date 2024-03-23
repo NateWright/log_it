13f2752516a0cba198fdd8d78affddf0c42d1dc5
Nathan Wright <nathanwrightbusiness@gmail.com>
2024-03-23 16:29:07 +0000

    log_test

--- test/log_test.dart
+++ test/log_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:log_it/src/log_feature/log.dart';
 
@@ -8,11 +9,16 @@ void main() {
     'constructor',
     () {
       test('default', () {
+        final startTime = TimeOfDay(hour: 9, minute: 0);
         sut = Log(
           title: 'Test',
           description: 'Test Description',
           dataType: DataType.number,
           unit: 'lb',
+          hasNotifications: false,
+          dateRange: DateTimeRange(start: DateTime.now(), end: DateTime.now()),
+          interval: TimeInterval(10, TimeIntervalUnits.days),
+          startTime: startTime,
         );
       });
     },
