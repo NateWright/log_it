c93f7d8e0d3148921c9ccff0dd060b1b0614a227
theknackattack <mjads311@gmail.com>
2024-04-18 14:43:49 -0400

    Deleted a test.

--- test/notification_service_test.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: unused_import, unused_local_variable
-
-import 'package:flutter_test/flutter_test.dart';
-import 'package:log_it/src/notification_service/notification_service.dart';
-
-void main() {
-  late NotificationService sut;
-
-}
