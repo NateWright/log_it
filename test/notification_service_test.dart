db5ed18fa423c987ef3e2b079c75d59419022592
theknackattack <mjads311@gmail.com>
2024-04-18 13:32:38 -0400

    Notification service test.

---
+++ test/notification_service_test.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: unused_import, unused_local_variable
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:log_it/src/notification_service/notification_service.dart';
+
+void main() {
+  late NotificationService sut;
+
+}
